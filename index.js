d857834ac09aee90ba5811fa050d9ea41ddc6070
David Eldridge <d.vincenteldridge@gmail.com>
2021-02-24 18:48:24 -0500

    got to license question

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    { 
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What short of license should your project have?',
+        choices: ['Apache License 2.0', 'GNU General Public License 3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+    },
+    ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
@@ -13,43 +42,3 @@ function init() { }
 init();
 
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            name: 'location',
-            message: 'Where are you from?',
-        },
-        {
-            type: 'input',
-            name: 'hobby',
-            message: 'What is your favorite hobby?',
-        },
-        {
-            type: 'input',
-            name: 'food',
-            message: 'What is your favorite food?',
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
-        },
-        {
-            type: 'input',
-            name: 'linkedin',
-            message: 'Enter your LinkedIn URL.',
-        },
-    ])
-    .then((answers) => {
-        const htmlPageContent = generateHTML(answers);
-
-        fs.writeFile('index.html', htmlPageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created index.html!')
-        );
-    });
