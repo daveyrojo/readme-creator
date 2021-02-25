1ee41692f3528ef54156010377fb97d674db62a2
David Eldridge <d.vincenteldridge@gmail.com>
2021-02-25 18:07:22 -0500

    starting license code

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'What short of license should your project have?',
-        choices: ['Apache License 2.0', 'GNU General Public License 3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'None']
+        choices: ['Apache License 2.0', 'MIT License', 'BSD 2-Clause', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'None']
     },
     {
         type: 'input',
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,46 @@
+//need to add license near top
+//needed section called questions
+//has link to gitub profile
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+var renderedLicense;
 function renderLicenseBadge(license) {}
-
 // TODO: Create a function that returns the license link
+
+if ( `${data.license}` === 'Apache License 2.0') {
+
+  renderedLicense = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+
+  return renderedLicense;
+
+} else if (`${data.license}` === 'BSD 3-Clause "New" or "Revised" License') {
+
+  renderedLicense = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+
+  return renderedLicense;
+
+} else if (`${data.license}` === 'MIT License') {
+
+  renderedLicense = 'MIT[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+
+  return renderedLicense;
+
+} else if (`${data.license}` === 'Boost Software License 1.0') {
+
+  renderedLicense = 'Boost[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+
+  return renderedLicense;
+
+} else {
+
+  renderedLicense = '';
+
+  return renderedLicense;
+
+}
+
+//${data.license} +=???
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
@@ -17,7 +55,7 @@ function generateMarkdown(data) {
 
     ## Description
     ``` `${data.description}` ```
-
+    //license badge goes here
     ## Installation
     ``` `${data.dependencies}` ```
 
@@ -30,7 +68,16 @@ function generateMarkdown(data) {
     ## Tests
     ``` `${data.tests}` ```
 
+    ## Questions
+    ``` `${data.email}` ```
+    ``` `[${data.github}](https://github.com/${data.github})` ```
+
   `;
 }
 
 module.exports = generateMarkdown;
+
+
+
+
+
